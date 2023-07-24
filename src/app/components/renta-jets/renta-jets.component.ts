ea90420f17500ca2353da88915b2020e52d918cf
itzelpalre <itzelpalmareyes@gmail.com>
2023-07-24 17:03:49 -0600

    Pantalla Card Jets

--- src/app/components/renta-jets/renta-jets.component.ts
+++ src/app/components/renta-jets/renta-jets.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class RentaJetsComponent {
 
+  jets = [
+    {
+      title: 'Gulfstream G450',
+      img: '../../assets/imgs/JETS/exportados/Gulfstream_G450.png'
+    },
+    {
+      title: 'Cessna Citation III',
+      img: '../../assets/imgs/JETS/exportados/Cessna_Citation_III.png'
+    },
+    {
+      title: 'Falcon 200',
+      img: '../../assets/imgs/JETS/exportados/Falcon_200.png'
+    },
+    {
+      title: 'Cessna Citation II',
+      img: '../../assets/imgs/JETS/exportados/Cessna_Citation_II.png'
+    },
+    {
+      title: 'Hawker 800',
+      img: '../../assets/imgs/JETS/exportados/Hawker.png'
+    },
+    {
+      title: 'Learjet 45',
+      img: '../../assets/imgs/JETS/exportados/Learjet_45.png'
+    },
+    {
+      title: 'Chalenger CL-600',
+      img: '../../assets/imgs/JETS/exportados/Chalenger_CL-600.png'
+    },
+    {
+      title: 'Learjet 35A',
+      img: '../../assets/imgs/JETS/exportados/Learjet_35A.png'
+    },
+  ];
+
 }
